522b86f713bc0e41a25061ef9775d970edbc5514
Artica <Artica@DESKTOP-LVP1P1M>
2024-10-12 10:54:38 -0400

    "codigo unico"

--- lib/features/auth/data/auth_service.dart
+++ lib/features/auth/data/auth_service.dart
@@ -1,10 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:math'; // Para generar el número aleatorio
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Función para generar un código único con prefijo "EZPC" y 10 dígitos
+  String generateUniqueCode() {
+    final random = Random();
+    final code = List<int>.generate(6, (_) => random.nextInt(10))
+        .join(); // Genera 6 dígitos aleatorios
+    return 'EZPC$code'; // Código con prefijo "EZPC" + 6 dígitos aleatorios
+  }
 
   // Inicio de sesión con correo electrónico y contraseña
   Future<UserCredential?> signInWithEmailAndPassword(
@@ -16,8 +25,6 @@ class AuthService {
       );
       return userCredential;
     } on FirebaseAuthException catch (e) {
-      // Manejo de errores de autenticación
-      // Puedes mostrar un mensaje de error al usuario o lanzar una excepción personalizada
       print('Login error: ${e.message}');
       return null;
     }
@@ -42,7 +49,6 @@ class AuthService {
     required String email,
     required String name,
     required String lastName,
-    //   required DateTime dob,
     required String phoneNumber,
     required String username,
     required String password,
@@ -59,50 +65,48 @@ class AuthService {
       User? user = userCredential.user;
 
       if (user != null) {
+        // Generar el código único
+        String uniqueCode = generateUniqueCode();
+
         // Guardar información adicional en Firestore
         await _firestore.collection('users').doc(user.uid).set({
           'name': name,
           'lastName': lastName,
-          //         'dob': dob,
           'phoneNumber': phoneNumber,
           'role': role, // Guardar el rol obtenido
           'username': username,
           'email': email,
+          'uniqueCode': uniqueCode, // Guardar el código único
           'createdAt': FieldValue.serverTimestamp(),
         });
 
         return user;
       }
     } on FirebaseAuthException catch (e) {
-      // Manejo de errores de Firebase Auth
       print('Error: ${e.message}');
       return null;
     } catch (e) {
-      // Manejo de errores generales
       print('Error: $e');
       return null;
     }
     return null;
   }
 
+  // Obtener el rol del usuario desde Firestore
   Future<String?> getUserRole(User user) async {
     try {
-      // Get a reference to the user document in Firestore
       final userDoc = await FirebaseFirestore.instance
           .collection('users')
           .doc(user.uid)
           .get();
 
-      // Check if the document exists and has a 'role' field
       if (userDoc.exists && userDoc.data()!.containsKey('role')) {
         return userDoc.get('role');
       } else {
-        // Handle the case where the role is not found
         print('User role not found in Firestore');
         return null;
       }
     } catch (e) {
-      // Handle any errors that occur during the Firestore query
       print('Error fetching user role: $e');
       return null;
     }
