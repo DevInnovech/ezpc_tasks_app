8a17147636034464aee2ec584fe42c388d290cbe
Wheliver-Rivas <wheliver.rivas@innovechsoftware.com>
2025-02-08 11:40:51 -0400

    arreglo de key

--- lib/features/notification/generekey.dart
+++ lib/features/notification/generekey.dart
@@ -1,1 +1,24 @@
+import 'package:googleapis_auth/auth_io.dart';
+import 'dart:convert';
+import 'dart:io';
 
+class GetServerKey {
+  Future<String> getServerKeyToken() async {
+    // Definir los alcances necesarios para Firebase Cloud Messaging
+    final scopes = [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/firebase.database',
+      'https://www.googleapis.com/auth/firebase.messaging',
+    ];
+    final file = File('config/service-account.json');
+    final credentialsJson = jsonDecode(await file.readAsString());
+    // Cargar las credenciales de la cuenta de servicio
+    final credentials = ServiceAccountCredentials.fromJson(credentialsJson);
+
+    // Autenticación con la cuenta de servicio
+    final client = await clientViaServiceAccount(credentials, scopes);
+
+    // Retornar el token de autenticación
+    return client.credentials.accessToken.data;
+  }
+}
